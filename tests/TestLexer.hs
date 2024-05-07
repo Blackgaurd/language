5f822c7f12c0f07a4d10cddeabf2de8f6c76f69e
Bryan Deng <bryan.deng002@gmail.com>
2024-05-06 23:06:03 -0400

    change bracket to paren in test file

--- tests/TestLexer.hs
+++ tests/TestLexer.hs
@@ -23,29 +23,29 @@ tokenizeTests =
   , ("AbFwdd adja", [Ident "AbFwdd", Ident "adja", Eof])
   , ("1 + 2 * four - +orange  / 9", [Number "1", Add, Number "2", Mult, Ident "four", Sub, Add, Ident "orange", Div, Number "9", Eof])
   , ("when 1 + 2 == 3 then a = 1; otherwise {a = 2;}", [When, Number "1", Add, Number "2", Ee, Number "3", Then, Ident "a", Equal, Number "1", Semicolon, Otherwise, LBrace, Ident "a", Equal, Number "2", Semicolon, RBrace, Eof])
-  , ("(((1)))", [LBracket, LBracket, LBracket, Number "1", RBracket, RBracket, RBracket, Eof])
-  , ("(+1)-(+  2)", [LBracket, Add, Number "1", RBracket, Sub, LBracket, Add, Number "2", RBracket, Eof])
+  , ("(((1)))", [LParen, LParen, LParen, Number "1", RParen, RParen, RParen, Eof])
+  , ("(+1)-(+  2)", [LParen, Add, Number "1", RParen, Sub, LParen, Add, Number "2", RParen, Eof])
   , ("1 <= 2 < 3 > 4 >= 5 == 6 ~= 7 = 8", [Number "1", Le, Number "2", Lt, Number "3", Gt, Number "4", Ge, Number "5", Ee, Number "6", Ne, Number "7", Equal, Number "8", Eof])
-  , ("((1)) + (3 * (4 + 5))", [LBracket, LBracket, Number "1", RBracket, RBracket, Add, LBracket, Number "3", Mult, LBracket, Number "4", Add, Number "5", RBracket, RBracket, Eof])
-  , ("proc doNothing(x, y) { x = x + 1;y=y + 2; z=(x/y)*2;}", [Proc, Ident "doNothing", LBracket, Ident "x", Comma, Ident "y", RBracket, LBrace, Ident "x", Equal, Ident "x", Add, Number "1", Semicolon, Ident "y", Equal, Ident "y", Add, Number "2", Semicolon, Ident "z", Equal, LBracket, Ident "x", Div, Ident "y", RBracket, Mult, Number "2", Semicolon, RBrace, Eof])
-  , ("proc SUM (A, B){RETURN=A+B; return RETURN;}", [Proc, Ident "SUM", LBracket, Ident "A", Comma, Ident "B", RBracket, LBrace, Ident "RETURN", Equal, Ident "A", Add, Ident "B", Semicolon, Return, Ident "RETURN", Semicolon, RBrace, Eof])
+  , ("((1)) + (3 * (4 + 5))", [LParen, LParen, Number "1", RParen, RParen, Add, LParen, Number "3", Mult, LParen, Number "4", Add, Number "5", RParen, RParen, Eof])
+  , ("proc doNothing(x, y) { x = x + 1;y=y + 2; z=(x/y)*2;}", [Proc, Ident "doNothing", LParen, Ident "x", Comma, Ident "y", RParen, LBrace, Ident "x", Equal, Ident "x", Add, Number "1", Semicolon, Ident "y", Equal, Ident "y", Add, Number "2", Semicolon, Ident "z", Equal, LParen, Ident "x", Div, Ident "y", RParen, Mult, Number "2", Semicolon, RBrace, Eof])
+  , ("proc SUM (A, B){RETURN=A+B; return RETURN;}", [Proc, Ident "SUM", LParen, Ident "A", Comma, Ident "B", RParen, LBrace, Ident "RETURN", Equal, Ident "A", Add, Ident "B", Semicolon, Return, Ident "RETURN", Semicolon, RBrace, Eof])
   ]
 
-checkBracketsTests =
+checkParensTests =
   [ ([Eof], True)
   , ([], True)
-  , ([LBracket, Eof], False)
-  , ([RBracket, Eof], False)
-  , ([LBracket, Number "123", Add, Number "456", RBracket, Eof], True)
-  , ([LBracket, Ident "a", Eof], False)
-  , ([LBracket, LBracket, Number "1", RBracket, RBracket, Eof], True)
-  , ([LBracket, LBracket, Number "1", RBracket, RBracket, RBracket, Eof], False)
-  , ([LBracket, LBracket, LBracket, Number "1", RBracket, RBracket, Eof], False)
+  , ([LParen, Eof], False)
+  , ([RParen, Eof], False)
+  , ([LParen, Number "123", Add, Number "456", RParen, Eof], True)
+  , ([LParen, Ident "a", Eof], False)
+  , ([LParen, LParen, Number "1", RParen, RParen, Eof], True)
+  , ([LParen, LParen, Number "1", RParen, RParen, RParen, Eof], False)
+  , ([LParen, LParen, LParen, Number "1", RParen, RParen, Eof], False)
   , ([LBrace], False)
   , ([RBrace], False)
   , ([LBrace, RBrace], True)
-  , ([LBrace, LBracket, LBrace, RBrace, RBracket, RBrace], True)
-  , ([LBrace, LBrace, LBrace, RBracket, RBracket, RBrace], False)
+  , ([LBrace, LParen, LBrace, RBrace, RParen, RBrace], True)
+  , ([LBrace, LBrace, LBrace, RParen, RParen, RBrace], False)
   ]
 
 toTest testFunction x =
@@ -54,7 +54,7 @@ toTest testFunction x =
 
 tests =
   TestList
-    (map (toTest tokenize) tokenizeTests ++ map (toTest checkBrackets) checkBracketsTests)
+    (map (toTest tokenize) tokenizeTests ++ map (toTest checkParens) checkParensTests)
 
 main :: IO ()
 main = runTestTTAndExit tests
